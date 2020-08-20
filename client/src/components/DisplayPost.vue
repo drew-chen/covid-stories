a6a69a41e98ddfcec456e92f04fc25933a2c72d3
Andrew Chen <chen_andrew@berkeley.edu>
2020-08-20 01:05:00 -0700

    Create display for posts

---
+++ client/src/components/DisplayPost.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <article>
+      <h2>{{ post.title }}</h2>
+      <p>{{ post.body }}</p>
+    </article>
+    <section>Posted by {{ post.userId }}</section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+import { PostModel } from '../types/models'
+export default Vue.extend({
+  props: {
+    // If limit is -1, then there is no limit.
+    bodyViewLimit: {
+      type: Number,
+      default: -1
+    },
+    post: {
+      type: Object as PropType<PostModel>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
