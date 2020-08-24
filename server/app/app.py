54add1e7eff886a4d2cd783786bc4f41e6a16ea5
Andrew Chen <chen_andrew@berkeley.edu>
2020-08-24 02:24:41 -0700

    Create post and get routes for /posts

--- server/app/app.py
+++ server/app/app.py
@@ -26,3 +26,30 @@ class PostModel(db.Model):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/posts', methods=['POST', 'GET'])
+def handle_posts():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_post = PostModel(
+                title=data['title'],
+                body=data['body'],
+                userId=data['userId']
+            )
+            db.session.add(new_post)
+            db.session.commit()
+            return { "message": f"{new_post} has been created successfully." }
+        else:
+            return { "error": "The request payload is not in JSON format." }
+
+    elif request.method == 'GET':
+        query_results = PostModel.query.all()
+        posts = [{
+            "id": post.id,
+            "title": post.title,
+            "body": post.body,
+            "userId": post.userId
+        } for post in query_results]
+
+        return { "posts": posts }
